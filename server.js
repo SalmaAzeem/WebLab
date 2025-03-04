bcae00747d61f90cec4b613926e54ce828cb08d1
SalmaAzeem <s-salma.azeem@zewailcity.edu.eg>
2025-03-04 13:26:40 +0200

    add authentication middleware for book routes

--- server.js
+++ server.js
@@ -6,8 +6,20 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+const authMiddleware = (req, res, next) => {
+    const token = req.headers['authorization'];
+    if (token === 'Bearer ZEWAIL') {
+        next();
+    } else {
+        res.status(403).send('Forbidden: Invalid Token');
+    }
+};
+
+
+
 const bookRoutes = require('./routes/books');
-app.use('/books', bookRoutes);
+// check this
+app.use('/books', authMiddleware, bookRoutes);
 
 
 app
